91f0b361fccede9423e48259f10271199c23635b
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-07-30 02:44:33 +0000

    Finished post request.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -22,5 +22,26 @@ app.get('/api/grades', (req, res, next) => {
   });
 });
 
+app.post('/api/grades', (req, res, next) => {
+  const body = req.body;
+  const sql = `insert into "grades" ("name", "course", "score")
+               values ($1, $2, $3)
+               returning *`;
+  const score = Number(body.score);
+  const values = [body.name, body.course, body.score];
+  if (!body.name || !body.course || !body.score) {
+    return res.status(400).json({ Error: 'Missing name, course, or score' });
+  } else if (!Number.isInteger(score) || score > 100 || score < 0) {
+    return res.status(400).json({ Error: 'Score must be a positive integer from 0-100' });
+  }
+  db.query(sql, values).then(result => {
+    const newRow = result.rows;
+    res.status(201).json(newRow);
+  }).catch(err => {
+    console.error(err);
+    res.status(500).json({ Error: 'An unexpected error occurred' });
+  });
+});
+
 app.listen(3000, () => {
 });
