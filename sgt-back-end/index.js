384bdba16a1142eca2581c22cb187af88f041cd1
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-07-31 22:27:07 +0000

    Completed PUT response.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -43,5 +43,40 @@ app.post('/api/grades', (req, res, next) => {
   });
 });
 
+app.put('/api/grades/:gradeId', (req, res, next) => {
+  const gradeId = Number(req.params.gradeId);
+  const body = req.body;
+  const score = Number(body.score);
+  const sql = `
+    update "grades"
+       set "name" = $2,
+           "course" = $3,
+           "score" =$4
+     where "gradeId" = $1
+     returning *`;
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    return res.status(400).json({ error: '"gradeId" must be a positive integer' });
+  } else if (!body.name || !body.course || !body.score) {
+    return res.status(400).json({ Error: 'Missing name, course, or score' });
+  } else if (!Number.isInteger(score) || score > 100 || score < 0) {
+    return res.status(400).json({ Error: 'Score must be a positive integer from 0-100' });
+  }
+  const params = [gradeId, body.name, body.course, body.score];
+  db.query(sql, params).then(result => {
+    const grade = result.rows;
+    if (!grade) {
+      res.status(404).json({ error: `Cannot find grade with "gradeId" ${gradeId}` });
+    } else {
+      res.json(grade);
+    }
+  })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
 app.listen(3000, () => {
 });
